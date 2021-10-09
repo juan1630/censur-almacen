798ede5180baf36d87c8924d2e3373f99a3e1346
Juan Patrón <josejuanpatron1630@gmail.com>
2021-10-08 22:34:17 -0500

    Ruta en deploy

--- js/include.js
+++ js/include.js
@@ -2,7 +2,9 @@ var url = window.location.href;
 
 
 if( url.includes('localhost')  ) {
-    var swLocation = '../sw.js'
     
-}
+    var swLocation = '../sw.js'
+}else {
+        swLocation = '/censur-almacen/sw.js'
+}   
 navigator.serviceWorker.register( swLocation );
