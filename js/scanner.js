9b819a4ad0290bf4eb453f904cce0e4d14a1229d
Juan Patrón <josejuanpatron1630@gmail.com>
2021-10-13 10:50:15 -0500

    Se agrego el stop del scanner

--- js/scanner.js
+++ js/scanner.js
@@ -1,47 +1,48 @@
 
 if( localStorage.getItem('user')  != null ) {
 
-    var contador = 0;
+
     const html5QrcodeScanner = new Html5QrcodeScanner("reader", { fps: 10, qrbox: {width: 250, height: 250} }, false);
 
     const qrCodeSuccessCallback =  (decodedText, decodedResult) => {
 
-        if( contador < 5) {
-    
-            contador += 1;
-            if(decodedText.includes("https://blooming-sea-53514.herokuapp.com/")){
-    
-                fetch( decodedText, {
-                    method: 'POST'
-                } )
-                    .then(   resp => resp.json())
-                    .then( data => {
-                    if(data.ok ) {
-                        if(data.data.status != 'STOCK') {
-                            Swal.fire('El código ya ha sido usado', 'Intenta con otro codigo', 'error');
-                            window.location.href = '../index.html';
-                            return;
-                        }else {
-                            Swal.fire('Venta realizada','El producto se vendió', 'success');
-                            window.location.href = '../index.html';
-    
-                            return;
-                        }            
-                    }
-                });
+        if(decodedText){
+
+                // validamos que sea un codigo QR que pertenece a censur 
+                if(decodedText.includes("https://blooming-sea-53514.herokuapp.com/")){
+            // hacemos la peticion del tipo POST a la API
+                    fetch( decodedText, {
+                        method: 'POST'
+                    })
+                        .then(   resp => resp.json())
+                        .then( data => {
+                            // respuesta de la API
+                        if(data.ok ) {
+
+                            if(data.data.status != 'STOCK') {
+                                // validamos que el Qr tenga el status de vendido
+                                html5QrcodeScanner.stop()
+                                .then( resp => {
+                                    
+                                    Swal.fire('El código ya ha sido usado', 'Intenta con otro codigo', 'error');
+                                });
+                            }else {
+
+                                stopScanning();
+                            }            
+                        }
+                    });
+            
+                }else {
         
-            }else {
-    
-                Swal.fire('Codgio Qr invalido', 'Ingresa un código de censur', 'error');
-                window.location.href = '../index.html';
-                return;
-            }
-    
-        } else {
-    
-            Swal.fire('Número maximo de veces', 'Intenta de nuevo más tarde', 'error');
-            window.location.href = '../index.html';
-    
+                    Swal.fire('Codgio Qr invalido', 'Ingresa un código de censur', 'error');
+                    html5QrcodeScanner.stop()
+                    .then( resp =>{
+                        window.location.href = '../modules.html';
+                    })
+                }
+        
+            
         }
         
     };
@@ -53,3 +54,22 @@ if( localStorage.getItem('user')  != null ) {
     window.location.href = '../index.html';
 }
 
+
+
+const stopScanning = async () => {
+
+//hacemos el stock del scanner para que no se cicle
+    try {
+        const data = await html5QrcodeScanner.stop()
+
+        if(data) {
+            Swal.fire('Venta realizada','El producto se vendió', 'success');
+            window.location.href = '../index.html';
+        }
+    } catch (error) {
+        console.log(error);
+        window.location.href = '../index.html';
+    }
+
+   return
+}
